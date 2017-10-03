14aa3d0c1840f45297c0ff0f776a504597a95020
ConnorOLeary <themidlvlboss@gmail.com>
2017-10-03 09:15:17 -0600

    Added a popup display file

---
+++ src/useful/view/popupDisplay.java
@@ -0,0 +1,15 @@
+package useful.view;
+
+import javax.swing.JOptionPane;
+
+public class popupDisplay {
+	public void displayText(String message) {
+		JOptionPane.showMessageDialog(null, message);
+	}
+	public String getResponse(String fromQuestion) {
+		String response = "";
+		
+		response += JOptionPane.showInputDialog(null, fromQuestion);
+		return response;
+	}
+}
