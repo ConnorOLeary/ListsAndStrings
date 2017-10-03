25fef8560afb315a7b6050932a060c6949d3a724
ConnorOLeary <themidlvlboss@gmail.com>
2017-10-03 09:03:14 -0600

    Added Comments and created constructor for donut

--- src/useful/controller/ToolController.java
+++ src/useful/controller/ToolController.java
@@ -1,5 +1,20 @@
 package useful.controller;
 
-public class ToolController {
+import java.util.*;
+import useful.model.Donut;
 
+public class ToolController {
+	//Declares the donut List to store the donuts that we create
+	private List<Donut> donutList;
+	
+	public ToolController() {
+		donutList = new ArrayList<Donut>();
+	}
+	
+	public void start() {
+		//Creates a donut and adds it to the list
+		Donut temp = new Donut();
+		donutList.add(temp);
+		System.out.println(donutList);
+	}
 }
---
+++ src/useful/model/Donut.java
@@ -0,0 +1,15 @@
+package useful.model;
+
+public class Donut {
+	//Declares data members for the Donut constructor
+	private boolean hasSprinkles;
+	private boolean isGlazed;
+	private boolean hasFilling;
+	private String shape;
+	private String flavor;
+	private int holes;
+	
+	public Donut() {
+		
+	}
+}
